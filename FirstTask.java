b93f9037ebbf0036b22c70a6c045eb3db3ce5226
simeonmilevoev <simeonkrumov01@gmail.com>
2023-09-19 23:55:52 +0300

    Create FirstTask.java
    FirstCode

---
+++ FirstTask.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hello = 100 ;
+        String symbol = "%";
+        System.out.printf(" I am trying my best at %d %s ", hello, symbol);
+    }
+}
---
+++ out/production/myLearningInProgramming/README.md
@@ -0,0 +1,2 @@
+# myLearningInProgramming
+FirstProject
